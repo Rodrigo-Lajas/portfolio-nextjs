f7b805f573d28b8f93efe7f669cdeaa199ccd1a6
Rodrigo-Lajas <rodrigolajas18@gmail.com>
2025-03-19 20:39:21 -0300

    cambio de ubicacion en home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -22,12 +22,12 @@ export default function Page() {
     return (
         <section id="hero" className="bg-gray-800 text-white p-4 pt-12">
             <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-2">
+                <div data-aos="flip-down" className="flex flex-col items-center justify-center">
+                    <Image src="/images/IMG_5893.webp" width={800} height={800} alt="Hero Image" className="w-4/5 h-auto object-contain my-8 border-4 border-l-lime-400 border-t-lime-200 border-r-lime-200 border-b-lime-400 shadow-lg rounded-lg" loading='lazy' />
+                </div>
                 <div className="flex flex-col items-center justify-center m-4">
                     <h1 data-aos="fade-right" className="flex items-center justify-center m-4 text-4xl leading-normal font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-lime-400 to-lime-100 sm:text-6xl sm:leading-normal">Hola! Soy Rodrigo Lajas</h1>
                     <p data-aos="fade-left" className="flex items-center justify-center m-4 bg-clip-text text-transparent bg-gradient-to-r from-lime-400 to-lime-100 text-2xl font-bold leading-normal">Soy un desarrollador front-end en formación con pasión por crear aplicaciones web.</p>
-                </div>
-                <div data-aos="flip-down" className="flex flex-col items-center justify-center">
-                    <Image src="/images/IMG_5893.webp" width={800} height={800} alt="Hero Image" className="w-4/5 h-auto object-contain my-8 border-4 border-l-lime-400 border-t-lime-200 border-r-lime-200 border-b-lime-400 shadow-lg rounded-lg" loading='lazy' />
                 </div>
             </div>
             <div className="flex mx-auto items-center justify-center">
